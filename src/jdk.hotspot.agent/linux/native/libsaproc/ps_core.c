797b90d7e49c47047d243a50a4c976576d247b9c
Chris West (Faux) <git@goeswhere.com>
2016-01-01 22:52:16 +0000

    fix build warning in the worst way

--- src/jdk.hotspot.agent/linux/native/libsaproc/ps_core.c
+++ src/jdk.hotspot.agent/linux/native/libsaproc/ps_core.c
@@ -819,7 +819,8 @@ static bool read_exec_segments(struct ps_prochandle* ph, ELF_EHDR* exec_ehdr) {
       if (exec_php->p_filesz > BUF_SIZE) {
         goto err;
       }
-      pread(ph->core->exec_fd, interp_name, exec_php->p_filesz, exec_php->p_offset);
+      int read = pread(ph->core->exec_fd, interp_name, exec_php->p_filesz, exec_php->p_offset);
+
       interp_name[exec_php->p_filesz] = '\0';
       print_debug("ELF interpreter %s\n", interp_name);
       // read interpreter segments as well
